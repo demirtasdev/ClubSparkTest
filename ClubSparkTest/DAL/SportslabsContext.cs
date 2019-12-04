4076f4d99a65b839231b869de24baa1682adb69c
d3m1rt45 <d3m1rt45@outlook.com>
2019-12-04 11:30:06 +0000

    EF Set, Database Populated
    Context class and the model is set and the database is now populated.

---
+++ ClubSparkTest/DAL/SportslabsContext.cs
@@ -0,0 +1,19 @@
+﻿using ClubSparkTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ClubSparkTest.DAL
+{
+    public class SportslabsContext : DbContext
+    {
+        public SportslabsContext() : base("name=SportslabsContext")
+        {
+        }
+
+        public DbSet<Team> Teams { get; set; }
+    }
+
+}
---
+++ ClubSparkTest/Models/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClubSparkTest.Models
+{
+    public class Team
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public bool Eliminated { get; set; }
+    }
+}
